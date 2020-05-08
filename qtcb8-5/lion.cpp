16d28bc6ce8068dbe4f5164e0f2b2d0de3dd1480
raizen991 <33425942+GAlexandru14@users.noreply.github.com>
2020-05-08 11:41:20 +0300

    8-5. Overriding

---
+++ qtcb8-5/lion.cpp
@@ -0,0 +1,14 @@
+#include "lion.h"
+
+lion::lion(QObject *parent) : feline(parent)
+{
+
+}
+
+void lion::speak()
+{
+    qInfo() <<"ROAR!!!";
+
+    //call the function from the base class
+    feline::speak();
+}
