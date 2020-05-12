387252cc56e75b6d6b71cde1b72e029f57d4f3c2
raizen991 <33425942+GAlexandru14@users.noreply.github.com>
2020-05-12 22:00:14 +0300

    11-2. Connecting and disconnecting

---
+++ qtcb11-2/main.cpp
@@ -0,0 +1,66 @@
+#include <QCoreApplication>
+#include <QtDebug>
+#include <QTextStream>
+#include <iostream>
+#include "radio.h"
+#include "station.h"
+
+using namespace std;
+
+//11-2. Connecting and disconnecting
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    radio boombox;
+    station* channels[3];
+
+    //create some channels
+    channels[0] = new station(&boombox, 94, "Rock and Roll");
+    channels[1] = new station(&boombox, 101, "Hip Hop");
+    channels[2] = new station(&boombox, 85, "The News");
+
+    //Qt::QueuedConnection -- used when working with threads
+    boombox.connect(&boombox, &radio::quit, &a, &QCoreApplication::quit, Qt::QueuedConnection);
+
+    do {
+       qInfo() << "Enter on, off, test or quit:";
+       QTextStream qtin(stdin);
+       QString line = qtin.readLine().trimmed().toUpper();
+
+       if(line == "ON"){
+           qInfo() << "Turning the radio on";
+           for(int  i = 0; i < 3; i++){
+               station* channel = channels[i];
+               boombox.connect(channel, &station::send, &boombox, &radio::listen);
+           }
+           qInfo() << "Radio is on";
+       }
+       else if(line == "OFF"){
+           qInfo() << "Turning the radio off";
+           for(int  i = 0; i < 3; i++){
+               station* channel = channels[i];
+               boombox.disconnect(channel, &station::send, &boombox, &radio::listen);
+           }
+           qInfo() << "Radio is off";
+       }
+       else if(line == "TEST"){
+           qInfo() << "Testing";
+           for(int  i = 0; i < 3; i++){
+               station* channel = channels[i];
+               channel->broadcast("Broadcasting live: Video killed the radio star! :'(");
+           }
+           qInfo() << "Testing complete";
+       }
+       else if(line == "QUIT"){
+           qInfo() << "Quitting";
+           emit boombox.quit();
+           break;
+       }
+
+
+    }while(true);
+
+
+    return a.exec();
+}
