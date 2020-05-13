020c94675ec43ea7eefb0c5b12282fb9d46b1215
raizen991 <33425942+GAlexandru14@users.noreply.github.com>
2020-05-13 19:57:28 +0300

    14-3. Template classes

---
+++ qtcb14-3/main.cpp
@@ -0,0 +1,20 @@
+#include <QCoreApplication>
+#include <QDebug>
+#include "test.h"
+
+//14-3. Template classes
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    test<int> intCalc;
+    qInfo() << intCalc.add(1,4);
+
+    test<double> doubleCalc;
+    qInfo() << doubleCalc.add(1.09,0.075);
+
+    test<QString> stringCalc;
+    qInfo() << stringCalc.add("Hello ", "World");
+
+    return a.exec();
+}
