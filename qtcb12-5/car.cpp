0e7b3e98bfe243f586483c1374ddbfd991cd962c
raizen991 <33425942+GAlexandru14@users.noreply.github.com>
2020-05-13 09:50:33 +0300

    12-5. QObject_cast

---
+++ qtcb12-5/car.cpp
@@ -0,0 +1,17 @@
+#include "car.h"
+
+car::car(QObject *parent) : QObject(parent)
+{
+
+}
+
+void car::drive()
+{
+    qInfo() << "driving";
+
+}
+
+void car::stop()
+{
+    qInfo() << "stopping";
+}
